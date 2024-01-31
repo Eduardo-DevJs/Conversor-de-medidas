cb19b27568c11dedfee8fc75ed67ad03bf02679a
Eduardo-DevJs <dudev2001@gmail.com>
2024-01-31 14:33:09 -0300

    feature / clipboard

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
-import { FaExchangeAlt } from "react-icons/fa";
+import { FaExchangeAlt } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+import { FaCheck } from 'react-icons/fa';
+
+import { MdContentPaste } from 'react-icons/md';
 
 const Home = () => {
   const [pixels, setPixels] = useState<number>(0);
@@ -14,50 +17,72 @@ const Home = () => {
     setRem(remValue);
   }
 
+  function copyProp() {
+    const remString = rem.toString().replace(',', '.');
+    navigator.clipboard.writeText(remString + 'rem');
+
+    alert(`Unidade de medida ${remString}rem copiada com sucesso!`)
+  }
+
   return (
     <div className="bg-zinc-800 flex items-center justify-center h-screen">
-      <form className="md:flex flex-col items-center gap-5">
-        <h1 className="md:text-5xl text-white mb-5 font-medium">
-          Converter: PX / REM
-        </h1>
+      <div className="flex flex-col items-center gap-10">
+        <form className="md:flex flex-col items-center gap-5">
+          <h1 className="md:text-5xl text-white mb-5 font-medium">
+            Converter: PX / REM
+          </h1>
 
-        <div className="flex gap-5 items-center">
-          <div>
-            <label
-              className="md:block mb-3 text-center text-white font-medium text-2xl"
-              htmlFor=""
-            >
-              Pixels
-            </label>
-            <input
-              className="md:text-3xl text-center bg-transparent border text-white font-semibold p-5 rounded outline-none"
-              type="number"
-              onChange={pxToRem}
-              value={pixels}
-            />
+          <div className="flex gap-5 items-center">
+            <div>
+              <label
+                className="md:block mb-3 text-center text-white font-medium text-2xl"
+                htmlFor=""
+              >
+                Pixels
+              </label>
+              <input
+                className="md:text-3xl text-center bg-transparent border text-white font-semibold p-5 rounded outline-none"
+                type="number"
+                onChange={pxToRem}
+                value={pixels}
+              />
+            </div>
+
+            <Link to={'/rem-to-px'} className="mt-10">
+              <FaExchangeAlt size={30} color="#FFF" />
+            </Link>
+
+            <div>
+              <label
+                className="md:block mb-3 text-center text-white font-medium text-2xl"
+                htmlFor=""
+              >
+                REM
+              </label>
+              <input
+                className="md:text-3xl text-center bg-transparent border text-cyan-600 font-semibold p-5 rounded outline-none"
+                readOnly
+                type="number"
+                value={rem}
+              />
+            </div>
           </div>
+        </form>
 
-          <Link to={"/rem-to-px"} className='mt-10'>
-            <FaExchangeAlt size={30} color='#FFF'/>
-          </Link>
+        <div className="relative bg-zinc-900 p-5 w-full">
+          <h2 className="md:text-white font-bold text-2xl">
+            font-size: {rem}
+            <span className="text-yellow-300">rem</span>;
+          </h2>
 
-          <div>
-            <label
-              className="md:block mb-3 text-center text-white font-medium text-2xl"
-              htmlFor=""
-            >
-              REM
-            </label>
-            <input
-              className="md:text-3xl text-center bg-transparent border text-cyan-600 font-semibold p-5 rounded outline-none"
-              readOnly
-              type="number"
-              value={rem}
-            />
-          </div>
+          <MdContentPaste
+            onClick={copyProp}
+            size={25}
+            color="#FFF"
+            className="absolute cursor-pointer top-6 right-5"
+          />
         </div>
-        <h3 className='text-white mt-5 font-bold uppercase'>Feito por Dudu Oliveira - 2024</h3>
-      </form>
+      </div>
     </div>
   );
 };
--- src/RemToPx/RemToPx.tsx
+++ src/RemToPx/RemToPx.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { FaExchangeAlt } from 'react-icons/fa';
+import { MdContentPaste } from 'react-icons/md';
 import { Link } from 'react-router-dom';
 
 const RemToPx = () => {
@@ -14,52 +15,72 @@ const RemToPx = () => {
     setRem(pxValue);
   }
 
+  function copyProp() {
+    const pxString = rem.toString().replace(',', '.');
+    navigator.clipboard.writeText(pxString + 'px');
+
+    alert(`Unidade de medida ${pxString}px copiada com sucesso!`)
+  }
+
   return (
     <div className="bg-zinc-800 flex items-center justify-center h-screen">
-      <form className="md:flex flex-col items-center gap-5">
-        <h1 className="md:text-5xl text-white mb-5 font-medium">
-          Converter: REM / PX
-        </h1>
+      <div className='flex flex-col items-center gap-10'>
+        <form className="md:flex flex-col items-center gap-5">
+          <h1 className="md:text-5xl text-white mb-5 font-medium">
+            Converter: REM / PX
+          </h1>
 
-        <div className="md:flex gap-5 items-center">
-          <div>
-            <label
-              className="md:block mb-3 text-center text-white font-medium text-2xl"
-              htmlFor=""
-            >
-              REM
-            </label>
-            <input
-              className="md:text-3xl text-center bg-transparent border text-white font-semibold p-5 rounded outline-none"
-              type="number"
-              onChange={RemToPx}
-              value={pixels}
-            />
+          <div className="md:flex gap-5 items-center">
+            <div>
+              <label
+                className="md:block mb-3 text-center text-white font-medium text-2xl"
+                htmlFor=""
+              >
+                REM
+              </label>
+              <input
+                className="md:text-3xl text-center bg-transparent border text-white font-semibold p-5 rounded outline-none"
+                type="number"
+                onChange={RemToPx}
+                value={pixels}
+              />
+            </div>
+
+            <Link to={'/'} className="mt-10">
+              <FaExchangeAlt size={30} color="#FFF" />
+            </Link>
+
+            <div>
+              <label
+                className="md:block mb-3 text-center text-white font-medium text-2xl"
+                htmlFor=""
+              >
+                Pixels
+              </label>
+              <input
+                className="md:text-3xl text-center bg-transparent border text-cyan-600 font-semibold p-5 rounded outline-none"
+                readOnly
+                type="number"
+                value={rem}
+              />
+            </div>
           </div>
+        </form>
 
-          <Link to={'/'} className="mt-10">
-            <FaExchangeAlt size={30} color="#FFF" />
-          </Link>
+        <div className="relative bg-zinc-900 p-5 w-full">
+          <h2 className="md:text-white font-bold text-2xl">
+            font-size: {rem}
+            <span className="text-yellow-300">px</span>;
+          </h2>
 
-          <div>
-            <label
-              className="md:block mb-3 text-center text-white font-medium text-2xl"
-              htmlFor=""
-            >
-              Pixels
-            </label>
-            <input
-              className="md:text-3xl text-center bg-transparent border text-cyan-600 font-semibold p-5 rounded outline-none"
-              readOnly
-              type="number"
-              value={rem}
-            />
-          </div>
+          <MdContentPaste
+            onClick={copyProp}
+            size={25}
+            color="#FFF"
+            className="absolute cursor-pointer top-6 right-5"
+          />
         </div>
-        <h3 className="text-white mt-5 font-bold uppercase">
-          Feito por Dudu Oliveira - 2024
-        </h3>
-      </form>
+      </div>
     </div>
   );
 };
